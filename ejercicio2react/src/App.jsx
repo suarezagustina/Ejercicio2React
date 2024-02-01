485d7e5c64132c0bc00cea3cf84758a0d84dd4e2
Suarez Agustina <suarezagustina12.12@gmail.com>
2024-02-01 00:41:14 -0300

    utilice el objeto props y mande un mensaje al texto hello, hice el readme

--- ejercicio2react/src/App.jsx
+++ ejercicio2react/src/App.jsx
@@ -1,9 +1,12 @@
-function App() {
- 
+import Ejercicio2 from "./components/Ejercicio2";
+import 'bootstrap/dist/css/bootstrap.min.css'
 
+function App() {
   return (
-  <h1>Ejercicio 2</h1>
-  )
+  <section>
+    <h1>Ejercicio 2</h1>
+    <Ejercicio2 mensaje='My friend'/>
+    </section> );
 }
 
-export default App
+export default App;
---
+++ ejercicio2react/src/components/Ejercicio2.jsx
@@ -0,0 +1,9 @@
+const Ejercicio2 = ({mensaje}) => {
+    return (
+        <section>
+            <h1 className="display-1 text-center">Hello {mensaje}</h1>
+        </section>
+    );
+};
+
+export default Ejercicio2;
